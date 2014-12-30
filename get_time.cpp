fd4ca26ab4728efac5c7438325f690a2d9773c3b
David C Black <dcblack@hldwizard.com>
2014-12-30 16:29:02 +0000

    add selftest

--- get_time.cpp
+++ get_time.cpp
@@ -1,5 +1,10 @@
+// To self-test:
+// % g++ -DGET_TIME_SELFTEST -o get_time get_time.cpp && ./get_time 24
+#ifndef GET_TIME_SELFTEST
 #include "svdpi.h"
+#endif
 
+////////////////////////////////////////////////////////////////////////////////
 //  Windows
 #ifdef _WIN32
 #include <Windows.h>
@@ -16,7 +21,7 @@ double get_wall_time(void)
     //  Handle error
     return 0;
   }
-  return (double)time.QuadPart / freq.QuadPart;
+  return double(time.QuadPart) / freq.QuadPart;
 }
 
 extern "C"
@@ -27,7 +32,7 @@ double get_cpu_time(void)
     //  Returns total user time.
     //  Can be tweaked to include kernel times as well.
     return
-      (double)(d.dwLowDateTime |
+      double(d.dwLowDateTime |
       ((unsigned long long)d.dwHighDateTime << 32)) * 0.0000001;
   }else{
     //  Handle error
@@ -35,6 +40,7 @@ double get_cpu_time(void)
   }
 }
 
+////////////////////////////////////////////////////////////////////////////////
 //  Posix/Linux
 #else
 #include <time.h>
@@ -48,12 +54,55 @@ double get_wall_time(void)
     //  Handle error
     return 0;
   }
-  return (double)time.tv_sec + (double)time.tv_usec * .000001;
+  return double(time.tv_sec) + double(time.tv_usec) * .000001;
 }
 
 extern "C"
 double get_cpu_time(void)
 {
-  return (double)clock() / CLOCKS_PER_SEC;
+  return double(clock()) / CLOCKS_PER_SEC;
 }
 #endif
+////////////////////////////////////////////////////////////////////////////////
+#ifdef GET_TIME_SELFTEST
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <sstream>
+#include <stdint.h>
+using namespace std;
+uint64_t mask(uint64_t value) { return value & 0xFFFFFFFFULL; }
+int main(int argc, char* argv[])
+{
+  // Defaults
+  unsigned int bits = 28; //< default
+  // Parse command-line
+  for (int i=1; i<argc; ++i) {
+    string arg(argv[i]);
+    if (arg.length() > 0 && arg[0] > '0' && arg[0] <= '9') {
+      istringstream strin(arg);
+      unsigned int temp = bits;
+      strin >> temp;
+      if (temp > 1 && temp <= 32) {
+        bits = temp;
+        cout << "Using " << bits << endl;
+      }
+    }
+  }
+
+  double wall_start = get_wall_time();
+  double cpu_start = get_cpu_time();
+
+  // Do a dummy calculation to cause time elapse
+  uint64_t csum = 0xDEADBEEFULL;
+  for (uint64_t i = 0ULL; i!=(1ULL<<bits)-1ULL; ++i) {
+    csum = mask(csum >> 32) ^ mask(csum) ^ i;
+  }
+  cout << "Sum: " << hex << csum << dec << endl;
+  cout << "CPU  time: " << get_cpu_time() - cpu_start << endl;
+  cout << "Wall time: " << get_wall_time() - wall_start << endl;
+  return 0;
+}
+#endif
+////////////////////////////////////////////////////////////////////////////////
+//EOF
