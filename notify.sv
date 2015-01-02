9b15435b51298b10425ac094a3791655c494e4e9
David C Black <dcblack@hldwizard.com>
2015-01-02 12:15:42 +0000

    tweak

--- notify.sv
+++ notify.sv
@@ -1,19 +1,20 @@
+// Send e-mail notification at the end of simulation
 `include "get_env.svh"
 module Notify;
   timeunit 1ps;
   timeprecision 1ps;
   final begin
     string email;
-    string job;
-    string file;
+    string subject;
+    string mailfile;
     string cmnd;
     email = get_env("EMAIL");
     if (email != "") begin
-      job = get_env("JOB");
-      if (job   == "") job = "simulation";
-      file = get_env("MESSAGE");
-      if (file   == "") file = "/dev/null";
-      cmnd = $sformatf("mail -s 'Finished %s' %s <%s",job,email,file);
+      subject = get_env("JOB");
+      if (subject   == "") subject = "simulation";
+      mailfile = get_env("MAILFILE");
+      if (mailfile   == "") mailfile = "/dev/null";
+      cmnd = $sformatf("mail -s 'Finished %s' %s <%s",subject,email,mailfile);
       $system(cmnd);
     end
   end
