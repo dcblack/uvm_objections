a9417d441c149bd551dd6ead1e5aad24b7b9bb7f
David C Black <dcblack@hldwizard.com>
2015-01-10 11:11:07 -0600

    Formatting/comments

--- get_time.svh
+++ get_time.svh
@@ -1,5 +1,7 @@
 `ifndef GET_TIME_SVH
 `define GET_TIME_SVH
-import "DPI-C" pure get_cpu_time = function real get_cpu_time();
-import "DPI-C" pure get_wall_time = function real get_wall_time();
+import "DPI-C" pure get_cpu_time  // elapsed in CPU seconds
+                    = function real get_cpu_time();
+import "DPI-C" pure get_wall_time // wall clock in seconds
+                    = function real get_wall_time();
 `endif
