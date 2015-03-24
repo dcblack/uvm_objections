9b052bf2313a8314c9ed2848e3febeb9b4f9ab31
David C Black <dcblack@hldwizard.com>
2015-03-24 08:43:46 -0500

    Remove "pure" designation

--- get_time.svh
+++ get_time.svh
@@ -1,7 +1,9 @@
 `ifndef GET_TIME_SVH
 `define GET_TIME_SVH
-import "DPI-C" pure get_cpu_time  // elapsed in CPU seconds
+
+  import "DPI-C" get_cpu_time  // elapsed in CPU seconds
                     = function real get_cpu_time();
-import "DPI-C" pure get_wall_time // wall clock in seconds
+  import "DPI-C" get_wall_time // wall clock in seconds
                     = function real get_wall_time();
+
 `endif
